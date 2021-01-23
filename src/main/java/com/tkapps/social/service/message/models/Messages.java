b91e9daefc078c6da86a584b309b61a5e211f6ac
tkmoney11 <tylerkim@outlook.com>
2021-01-22 23:46:01 -0800

    First Commit

---
+++ src/main/java/com/tkapps/social/service/message/models/Messages.java
@@ -0,0 +1,14 @@
+package com.tkapps.social.service.message.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Messages {
+}
