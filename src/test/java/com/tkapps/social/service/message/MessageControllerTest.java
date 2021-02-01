37709460e8c2432c9a678b2c7329d286dee9c524
tkmoney11 <tylerkim@outlook.com>
2021-01-31 20:51:10 -0800

    Added unit tests for MessageControllerTest.java

--- src/test/java/com/tkapps/social/service/message/MessageControllerTest.java
+++ src/test/java/com/tkapps/social/service/message/MessageControllerTest.java
@@ -1,6 +1,12 @@
 package com.tkapps.social.service.message;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.hamcrest.Matchers.hasSize;
 
 import com.tkapps.social.service.message.controller.MessageController;
 import com.tkapps.social.service.message.models.Message;
@@ -14,14 +20,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(MessageController.class)
@@ -39,20 +44,31 @@ public class MessageControllerTest {
     @BeforeEach
     public void setup() { this.mockMvc = MockMvcBuilders.standaloneSetup(messageController).build(); }
 
-    Message mockMessage1 = new Message(0, "Hey");
-    Message mockMessage2 = new Message(1, "Wassup");
+    Message mockMessage0 = new Message(0, "Hey");
+    Message mockMessage1 = new Message(1, "Wassup");
 
     List<Message> messages = new ArrayList<>();
 
-    /**
-     * Tests to make sure MockMVC works
-     * @throws Exception
-     */
     @Test
-    void testMockMVC() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/message/"))
-                .andExpect(status().isOk());
-//                .andExpect(view().name("message")); TODO figure out what this is
+    void testFindAll() throws Exception {
+        messages.add(mockMessage0);
+        messages.add(mockMessage1);
+        Mockito.when(messageService.findAll()).thenReturn(messages);
+        ResultActions actions = mockMvc.perform(get("/message/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+
+        // TODO create more robust testing: add testing to see if fields exist.
+        // https://stackoverflow.com/questions/15371022/springmvc-mockmvc-jsonpath-compare-list-of-strings
     }
 
+    @Test
+    void testFindByMessageId() throws Exception {
+        Mockito.when(messageService.findByMessageId(0)).thenReturn(mockMessage0);
+        ResultActions actions = mockMvc.perform(get("/message/0"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.messageId",is(0)));
+    }
 }
